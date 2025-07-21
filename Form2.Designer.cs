e3c96403f0f8e3aa85f06a39ea7c1556ab3736e3
pablocavalcante <pablocavalcante@prodam.sp.gov.br>
2025-07-21 12:01:18 -0300

    Alteração Form2

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -35,11 +35,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(71, 103);
+            this.label1.Location = new System.Drawing.Point(106, 104);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 24);
+            this.label1.Size = new System.Drawing.Size(70, 24);
             this.label1.TabIndex = 0;
-            this.label1.Text = "BOLSONARO";
+            this.label1.Text = "Form2";
             // 
             // Form2
             // 
